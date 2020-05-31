d40cc766a8534a3a35308472f3c5423ebbb1a065
RANDOM-V2N5q <44544007+RANDOM-V2N5q@users.noreply.github.com>
2020-05-31 19:41:12 +0200

    Add form templates

--- ProjektSemestrIV/ViewModels/MainViewModel.cs
+++ ProjektSemestrIV/ViewModels/MainViewModel.cs
@@ -1,6 +1,22 @@
-﻿using System;
+﻿using ProjektSemestrIV.Commands;
+using System;
+using System.Windows.Input;
 
 namespace ProjektSemestrIV.ViewModels {
     class MainViewModel : BaseViewModel {
-    }
+		private BaseViewModel selectedViewModel;
+		public BaseViewModel SelectedViewModel {
+			get { return selectedViewModel; }
+			set {
+				selectedViewModel = value;
+				onPropertyChanged(nameof(SelectedViewModel));
+			}
+		}
+
+		public MainViewModel() {
+			UpdateFormView = new UpdateFormViewCommand(this);
+		}
+
+		public ICommand UpdateFormView { get; set; }
+	}
 }
