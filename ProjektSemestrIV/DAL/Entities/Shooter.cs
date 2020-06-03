3eebaafbda342fbe70c529db5722ed751dcfcca2
RANDOM-V2N5q <44544007+RANDOM-V2N5q@users.noreply.github.com>
2020-06-03 20:34:17 +0200

    Add Shooter entity and repository

---
+++ ProjektSemestrIV/DAL/Entities/Shooter.cs
@@ -0,0 +1,31 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjektSemestrIV.DAL.Entities {
+    public class Shooter {
+        public Int32? ID { get; set; }
+        public String Name { get; set; }
+        public String Surname { get; set; }
+
+        public Shooter( MySqlDataReader reader ) {
+            ID = Int32.Parse(reader["id"].ToString());
+            Name = reader["imie"].ToString();
+            Surname = reader["nazwisko"].ToString();
+        }
+
+        public Shooter(String name, String surname ) {
+            ID = null;
+            Name = name;
+            Surname = surname;
+        }
+
+        public string ToInsert() {
+            return $"('{Name}', '{Surname}')";
+        }
+    }
+}
---
+++ ProjektSemestrIV/DAL/Repositories/ShooterRepository.cs
@@ -0,0 +1,61 @@
+﻿using MySql.Data.MySqlClient;
+using ProjektSemestrIV.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjektSemestrIV.DAL.Repositories {
+    class ShooterRepository {
+        public static Boolean AddShooterToDatabase(Shooter shooter ) {
+            Boolean executed = false;
+            using(MySqlConnection connection = DatabaseConnection.Instance.Connection) {
+                MySqlCommand command = new MySqlCommand($"INSERT INTO `Zawody`.`strzelec` (`imie`, `nazwisko`) VALUES {shooter.ToInsert()}", connection);
+                connection.Open();
+                command.ExecuteNonQuery();
+                executed = true;
+                connection.Close();
+            }
+            return executed;
+        }
+
+        public static bool EditShooterInDatabase( Shooter shooter, Int32 id ) {
+            Boolean executed = false;
+            using(MySqlConnection connection = DatabaseConnection.Instance.Connection) {
+                MySqlCommand command = new MySqlCommand($"UPDATE `Zawody`.`strzelec` SET `imie` = '{shooter.Name}', `nazwisko` = '{shooter.Surname}' WHERE (`id` = '{id}')", connection);
+                connection.Open();
+                command.ExecuteNonQuery();
+                executed = true;
+                connection.Close();
+            }
+            return executed;
+        }
+
+        public static List<Shooter> GetAllShooters() {
+            List<Shooter> shooters = new List<Shooter>();
+            using(MySqlConnection connection = DatabaseConnection.Instance.Connection) {
+                MySqlCommand command = new MySqlCommand("SELECT * FROM Zawody.strzelec", connection);
+                connection.Open();
+                MySqlDataReader reader = command.ExecuteReader();
+                while(reader.Read()) {
+                    shooters.Add(new Shooter(reader));
+                }
+                connection.Close();
+            }
+            return shooters;
+        }
+
+        public static Boolean DeleteShooterFromDatabase( Int32 shooterID ) {
+            Boolean executed = false;
+            using(MySqlConnection connection = DatabaseConnection.Instance.Connection) {
+                MySqlCommand command = new MySqlCommand($"DELETE FROM `Zawody`.`strzelec` WHERE (`id` = '{shooterID}')", connection);
+                connection.Open();
+                command.ExecuteNonQuery();
+                executed = true;
+                connection.Close();
+            }
+            return executed;
+        }
+    }
+}
