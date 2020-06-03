0c907995f68a1cce40db376c03e02bc8b549e1cb
RANDOM-V2N5q <44544007+RANDOM-V2N5q@users.noreply.github.com>
2020-06-03 20:39:57 +0200

    Add MainModel

---
+++ ProjektSemestrIV/Models/MainModel.cs
@@ -0,0 +1,42 @@
+﻿using ProjektSemestrIV.DAL.Entities;
+using ProjektSemestrIV.DAL.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjektSemestrIV.Models {
+    class MainModel {
+        public Boolean AddShooterToDatabase( Shooter shooter ) {
+            if(ShooterRepository.AddShooterToDatabase(shooter)) {
+                return true;
+            }
+            return false;
+        }
+
+        public Boolean DeleteShooterFromDatabase( Int32 shooterID ) {
+            if(ShooterRepository.DeleteShooterFromDatabase(shooterID)) {
+                return true;
+            }
+            return false;
+        }
+
+        public Boolean EditShooterInDatabase( Shooter shooter, Int32 id ) {
+            if(ShooterRepository.EditShooterInDatabase(shooter, id)) {
+                return true;
+            }
+            return false;
+        }
+
+        public ObservableCollection<Shooter> GetAllShooters() {
+            List<Shooter> shooters = ShooterRepository.GetAllShooters();
+            ObservableCollection<Shooter> shootersCollection = new ObservableCollection<Shooter>();
+            foreach(Shooter shooter in shooters) {
+                shootersCollection.Add(shooter);
+            }
+            return shootersCollection;
+        }
+    }
+}
