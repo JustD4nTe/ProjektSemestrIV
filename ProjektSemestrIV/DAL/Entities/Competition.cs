3351527784634478843c24a6b310b830fab861e9
Jan Rubel <jasiekrubel@gmail.com>
2020-06-03 21:52:12 +0200

    Added Entity and Repository for Competition

---
+++ ProjektSemestrIV/DAL/Entities/Competition.cs
@@ -0,0 +1,47 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjektSemestrIV.DAL.Entities
+{
+    class Competition
+    {
+        #region Properties
+        public UInt16 Id { get; set; } 
+        public string Location { get; set; }
+        public string StartDate { get; set; }
+        public string EndDate { get; set; }
+        #endregion
+
+        #region Constructors
+        public Competition(MySqlDataReader reader)
+        {
+            Id = UInt16.Parse(reader["id"].ToString());
+            Location = reader["miejsce"].ToString();
+            StartDate = reader["rozpoczecie"].ToString();
+            EndDate = reader["zakonczenie"].ToString();
+        }
+
+        public Competition(string location, string startDate, string endDate)
+        {
+            Id = default;
+            Location = location.Trim();
+            StartDate = startDate.Trim();
+            EndDate = string.IsNullOrWhiteSpace(endDate) ? null : endDate.Trim();
+        }
+        #endregion
+
+        public string ToInsert()
+        {
+            return $"{Location} {StartDate} {EndDate}";
+        }
+
+        public override string ToString()
+        {
+            return $"{Id} {Location} {StartDate} {EndDate}";
+        }
+    }
+}
--- ProjektSemestrIV/ViewModels/CompetitionsViewModel.cs
+++ ProjektSemestrIV/ViewModels/CompetitionsViewModel.cs
@@ -1,10 +1,19 @@
-﻿using System;
+﻿using ProjektSemestrIV.DAL.Entities;
+using ProjektSemestrIV.DAL.Repositories;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ProjektSemestrIV.ViewModels {
     class CompetitionsViewModel : BaseViewModel {
+        public ObservableCollection<Competition> Competitions { get; set; } = null;
+
+        public CompetitionsViewModel()
+        {
+            Competitions = new ObservableCollection<Competition>(CompetitionsRepository.GetAllCompetitions());
+        }
     }
 }
