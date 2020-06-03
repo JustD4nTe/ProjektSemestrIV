07d48fc2787b0149f3895dfab7fd501f07fec5c9
RANDOM-V2N5q <44544007+RANDOM-V2N5q@users.noreply.github.com>
2020-06-04 00:13:08 +0200

    Refactor ShooterModel

--- ProjektSemestrIV/Models/ShooterModel.cs
+++ ProjektSemestrIV/Models/ShooterModel.cs
@@ -9,34 +9,18 @@ using System.Threading.Tasks;
 
 namespace ProjektSemestrIV.Models {
     class ShooterModel {
-        public Boolean AddShooterToDatabase( Shooter shooter ) {
-            if(ShooterRepository.AddShooterToDatabase(shooter)) {
-                return true;
-            }
-            return false;
-        }
+        public Boolean AddShooterToDatabase( Shooter shooter )
+            => ShooterRepository.AddShooterToDatabase(shooter);
 
-        public Boolean DeleteShooterFromDatabase( UInt32 shooterID ) {
-            if(ShooterRepository.DeleteShooterFromDatabase(shooterID)) {
-                return true;
-            }
-            return false;
-        }
+        public Boolean DeleteShooterFromDatabase( UInt32 shooterID )
+            => ShooterRepository.DeleteShooterFromDatabase(shooterID);
 
-        public Boolean EditShooterInDatabase( Shooter shooter, UInt32 id ) {
-            if(ShooterRepository.EditShooterInDatabase(shooter, id)) {
-                return true;
-            }
-            return false;
-        }
+        public Boolean EditShooterInDatabase( Shooter shooter, UInt32 id )
+            => ShooterRepository.EditShooterInDatabase(shooter, id);
 
         public ObservableCollection<Shooter> GetAllShooters() {
             List<Shooter> shooters = ShooterRepository.GetAllShooters();
-            ObservableCollection<Shooter> shootersCollection = new ObservableCollection<Shooter>();
-            foreach(Shooter shooter in shooters) {
-                shootersCollection.Add(shooter);
-            }
-            return shootersCollection;
+            return new ObservableCollection<Shooter>(shooters);
         }
     }
 }
