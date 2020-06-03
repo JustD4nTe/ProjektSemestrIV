f620bf1fded0b2e837d66fab467afda9aac1282d
RANDOM-V2N5q <44544007+RANDOM-V2N5q@users.noreply.github.com>
2020-06-03 22:50:58 +0200

    Add relay command

---
+++ ProjektSemestrIV/ViewModels/BaseClass/RelayCommand.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ProjektSemestrIV.ViewModels {
+    class RelayCommand : ICommand {
+        readonly Action<object> execute;
+        readonly Predicate<object> canExecute;
+
+        public RelayCommand( Action<object> execute, Predicate<object> canExecute ) {
+            this.execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            this.canExecute = canExecute;
+        }
+
+        public bool CanExecute( object parameter ) {
+            return canExecute == null ? true : canExecute(parameter);
+        }
+
+        public event EventHandler CanExecuteChanged {
+            add {
+                if(canExecute != null) CommandManager.RequerySuggested += value;
+            }
+            remove {
+                if(canExecute != null) CommandManager.RequerySuggested -= value;
+            }
+        }
+
+        public void Execute( object parameter ) {
+            execute(parameter);
+        }
+    }
+}
