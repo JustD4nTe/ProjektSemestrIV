598dc4988b234421665a28703c90b9ddff89e952
Jan Rubel <jasiekrubel@gmail.com>
2020-06-04 00:15:36 +0200

    Added CompetitionModel

---
+++ ProjektSemestrIV/Models/CompetitionModel.cs
@@ -0,0 +1,15 @@
+﻿using ProjektSemestrIV.DAL.Entities;
+using ProjektSemestrIV.DAL.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjektSemestrIV.Models
+{
+    class CompetitionModel
+    {
+        public List<Competition> GetAllCompetitionsFromDB() => CompetitionsRepository.GetAllCompetitionsFromDB();
+    }
+}
