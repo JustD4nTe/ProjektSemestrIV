dab323acee7f43ba04410143a1187e382ed4637e
RANDOM-V2N5q <44544007+RANDOM-V2N5q@users.noreply.github.com>
2020-06-03 23:05:21 +0200

    Refactor ConnectionViewModel command

--- ProjektSemestrIV/ViewModels/ConnectionViewModel.cs
+++ ProjektSemestrIV/ViewModels/ConnectionViewModel.cs
@@ -8,14 +8,30 @@ using ProjektSemestrIV.Commands;
 
 namespace ProjektSemestrIV.ViewModels {
     class ConnectionViewModel : BaseViewModel {
-        public ConnectionViewModel() {
-            ConfirmData = new ConfirmDataCommand(this);
-        }
         public String ServerAddress { get; set; } = Properties.Settings.Default.ServerAddress;
         public UInt32 Port { get; set; } = Properties.Settings.Default.Port;
         public String Database { get; set; } = Properties.Settings.Default.Database;
         public String User { get; set; } = Properties.Settings.Default.User;
         public String Password { get; set; } = Properties.Settings.Default.Password;
-        public ICommand ConfirmData { get; set; }
+
+
+        private ICommand confirmData = null;
+        public ICommand ConfirmData {
+            get {
+                if(confirmData == null) {
+                    confirmData = new RelayCommand(ExecuteConfirmData, null);
+                }
+
+                return confirmData;
+            }
+        }
+        public void Execute( object parameter ) {
+            Properties.Settings.Default.ServerAddress = ServerAddress;
+            Properties.Settings.Default.Port = Port;
+            Properties.Settings.Default.Database = Database;
+            Properties.Settings.Default.User = User;
+            Properties.Settings.Default.Password = Password;
+            Properties.Settings.Default.Save();
+        }
     }
 }
