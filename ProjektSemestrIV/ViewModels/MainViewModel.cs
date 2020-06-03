5cdffedbeda79e80334c668a7c518942dfbc6b39
RANDOM-V2N5q <44544007+RANDOM-V2N5q@users.noreply.github.com>
2020-06-03 20:33:01 +0200

    Add MainModel property to MainViewModel

--- ProjektSemestrIV/ViewModels/MainViewModel.cs
+++ ProjektSemestrIV/ViewModels/MainViewModel.cs
@@ -1,9 +1,12 @@
 ﻿using ProjektSemestrIV.Commands;
+using ProjektSemestrIV.Models;
 using System;
 using System.Windows.Input;
 
 namespace ProjektSemestrIV.ViewModels {
     class MainViewModel : BaseViewModel {
+		public MainModel MainModel { get; private set; } = new MainModel();
+
 		private BaseViewModel selectedViewModel;
 		public BaseViewModel SelectedViewModel {
 			get { return selectedViewModel; }
